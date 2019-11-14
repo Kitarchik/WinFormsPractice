2e90f974110f1661ac2dcc720aa2667dacbd34c4
kitarchik <kitarchik@gmail.com>
2019-11-15 00:35:40 +0300

    Models added.

---
+++ WinFormsPractice/WinFormsPractice/Client.cs
@@ -0,0 +1,25 @@
+﻿namespace WinFormsPractice
+{
+    public class Client
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        public string PinCode { get; }
+        public int Money { get; private set; }
+        public Client(string name, string surname, string pinCode, int money)
+        {
+            Name = name;
+            Surname = surname;
+            PinCode = pinCode;
+            Money = money;
+        }
+
+        public void InsertMoney(int money)
+        {
+            if (money > 0 && money < 400000)
+            {
+                Money += money;
+            }
+        }
+    }
+}
---
+++ WinFormsPractice/WinFormsPractice/ClientRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WinFormsPractice
+{
+    public class ClientRepository
+    {
+        private List<Client> _clients;
+
+        public ClientRepository()
+        {
+            Initialize();
+        }
+
+        public Client GetClientByPin(string pin)
+        {
+            return _clients.FirstOrDefault(x => x.PinCode == pin);
+        }
+
+        private void Initialize()
+        {
+            _clients = new List<Client>
+            {
+                new Client("John", "Doe", "1234", 1000),
+                new Client("Китар", "Роман", "4581", 1000),
+                new Client("Сергей", "Никольский", "3567", 1000),
+            };
+        }
+    }
+}
